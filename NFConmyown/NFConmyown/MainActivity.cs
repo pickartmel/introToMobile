260ff4147612b48755742efe3584d6ba97416436
Melissa Pickart <500146972@fvtc.edu>
2018-04-22 14:49:09 -0500

    add all my files

---
+++ NFConmyown/NFConmyown/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Nfc;
+using Android.Views;
+using System;
+
+
+namespace NFConmyown
+{
+    [Activity(Label = "NFConmyown", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        NfcAdapter nfcAdapter;
+        ToggleButton tglReadWright;
+        TextView txtTagContent;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            nfcAdapter = NfcAdapter.GetDefaultAdapter(this);
+            tglReadWright = (ToggleButton)FindViewById(r.id.tglReadWright);
+            txtTagContent = (TextView)FindViewById(R.id.txtTagContent);
+            //test
+        }
+
+        public void tglReadWriteOnlick(View view)
+        {
+            txtTagContent.SetText(' ');
+        }
+
+
+    }
+}
+
