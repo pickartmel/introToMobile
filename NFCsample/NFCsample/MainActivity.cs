0798b08bddb6fc59c25f3a5a88fc730097a61cad
Melissa Pickart <500146972@fvtc.edu>
2018-05-02 21:01:54 -0500

    NFCProject

---
+++ NFCsample/NFCsample/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Nfc;
+using Poz1.NFCForms.Droid;
+using Poz1.NFCForms.Abstract;
+using Android.Content;
+
+namespace NFCsample
+{
+    [Activity(Label = "NFCsample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        public NfcAdapter NFCdevice;
+        public NfcForms x;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            NfcManager NfcManager = (NfcManager)Android.App.Application.Context.GetSystemService(Context.NfcService);
+            NFCdevice = NfcManager.DefaultAdapter;
+
+            Xamarin.Forms.DependencyService.Register<INfcForms, NfcForms>();
+            x = Xamarin.Forms.DependencyService.Get<INfcForms>() as NfcForms;
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ ResearchNFC/ResearchNFC/MainActivity.cs
@@ -0,0 +1,72 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Nfc;
+using Poz1.NFCForms.Droid;
+using System.Runtime.Remoting.Contexts;
+using Poz1.NFCForms.Abstract;
+using Android.Content;
+using Android.Content.PM;
+using System;
+
+namespace ResearchNFC
+{
+    [Activity(Label = "ResearchNFC", MainLauncher = true)]
+
+
+
+    public class MainActivity : Activity
+    {
+        public NfcAdapter NFCdevice;
+        public NfcForms x;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            NfcManager NfcManager = (NfcManager)Android.App.Application.Context.GetSystemService(Context.NfcService);
+            NFCdevice = NfcManager.DefaultAdapter;
+
+            Xamarin.Forms.DependencyService.Register<INfcForms, NfcForms>();
+            x = Xamarin.Forms.DependencyService.Get<INfcForms>() as NfcForms;
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            if (NFCdevice != null)
+            {
+                var intent = new Intent(this, GetType()).AddFlags(ActivityFlags.SingleTop);
+                NFCdevice.EnableForegroundDispatch
+                (
+                    this,
+                    PendingIntent.GetActivity(this, 0, intent, 0),
+                    new[] { new IntentFilter(NfcAdapter.ActionTechDiscovered) },
+                    new String[][] {new string[] {
+                            NFCTechs.Ndef,
+                        },
+                        new string[] {
+                            NFCTechs.MifareClassic,
+                        },
+                    }
+                );
+            }
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            NFCdevice.DisableForegroundDispatch(this);
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            x.OnNewIntent(this, intent);
+        }
+    }
+}
+
