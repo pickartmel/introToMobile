56eb84a40f7d81fcad27f449541d0b0f2f5383bc
Melissa Pickart <pickartmel@gmail.com>
2018-05-04 19:41:46 -0500

    Now we have a working rudementary point system - we dont have a working results page though

---
+++ FinalProject/FinalProject/Custom/CalculateAge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace FinalProject.Custom
+{
+    public class CalculateAge
+    {
+        public string MovieAge
+        {
+            get;
+            set;
+        }
+
+        public string MusicAge
+        {
+            get;
+            set;
+        }
+
+      //  public string CalAge()
+      //  {
+        //    decimal decMovieAge, decMusicAge;
+       // }
+
+        public CalculateAge()
+        {
+        }
+    }
+}
--- FinalProject/FinalProject/Views/MoviePage.xaml.cs
+++ FinalProject/FinalProject/Views/MoviePage.xaml.cs
@@ -7,6 +7,9 @@ namespace FinalProject.Views
 {
     public partial class MoviePage : ContentPage
     {
+        decimal newPoints;
+        decimal currentPoints;
+        
         public MoviePage()
         {
             InitializeComponent();
@@ -15,6 +18,48 @@ namespace FinalProject.Views
         void Handle_Clicked(object sender, System.EventArgs e)
         {
            Navigation.PushAsync(new MusicPage());
+        }
+
+        void IdentityTheif_Handled(object sender, System.EventArgs e)
+        {
+            decimal.TryParse(lblPoints.Text, out currentPoints);
+            newPoints = currentPoints +1 ;
+            lblPoints.Text = newPoints.ToString();
+        }
+
+        void IronMan3_Clicked(object sender, System.EventArgs e)
+        {
+            decimal.TryParse(lblPoints.Text, out currentPoints);
+            newPoints = currentPoints + 1;
+            lblPoints.Text = newPoints.ToString();
+        }
+
+        void Mummy_Clicked(object sender, System.EventArgs e)
+        {
+            decimal.TryParse(lblPoints.Text, out currentPoints);
+            newPoints = currentPoints + 1;
+            lblPoints.Text = newPoints.ToString();
+        }
+
+        void VanillaSky_Clicked(object sender, System.EventArgs e)
+        {
+            decimal.TryParse(lblPoints.Text, out currentPoints);
+            newPoints = currentPoints + 1;
+            lblPoints.Text = newPoints.ToString();
+        }
+
+        void OldSchool_Clicked(object sender, System.EventArgs e)
+        {
+            decimal.TryParse(lblPoints.Text, out currentPoints);
+            newPoints = currentPoints + 1;
+            lblPoints.Text = newPoints.ToString();
+        }
+
+        void Matrix_Clicked(object sender, System.EventArgs e)
+        {
+            decimal.TryParse(lblPoints.Text, out currentPoints);
+            newPoints = currentPoints + 1;
+            lblPoints.Text = newPoints.ToString();
         }
     }
 }
