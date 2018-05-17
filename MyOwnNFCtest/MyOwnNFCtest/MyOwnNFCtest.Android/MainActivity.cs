89bc89de783e568b76483cdcabda86ab30d1131e
Melissa Pickart <500146972@fvtc.edu>
2018-05-17 16:37:56 -0500

    almost perfect... button txt doesnt make me happy though

---
+++ MyOwnNFCtest/MyOwnNFCtest/MyOwnNFCtest.Android/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Nfc;
+using Poz1.NFCForms.Droid;
+using System.Runtime.Remoting.Contexts;
+using Poz1.NFCForms.Abstract;
+
+using Poz1.NFCForms.Droid;
+
+namespace MyOwnNFCtest.Droid
+{
+    [Activity(Label = "MyOwnNFCtest", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public NfcAdapter NFCdevice;
+        public NfcForms x;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            NfcManager NfcManager = (NfcManager)Android.App.Application.Context.GetSystemService(Context.NfcService);
+            NFCdevice = NfcManager.DefaultAdapter;
+
+            Xamarin.Forms.DependencyService.Register<INfcForms, NfcForms>();
+            x = Xamarin.Forms.DependencyService.Get<INfcForms>() as NfcForms;
+
+            LoadApplication(new App());
+        }
+    }
+}
+
